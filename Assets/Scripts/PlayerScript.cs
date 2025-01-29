3951477c843fc5a90834aa0bcb06c58e4f31d8aa
Divyanshu Divaker Novretee <divyanshuddn@gmail.com>
2025-01-29 19:12:54 +0530

    speed commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] private float speed=1000f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
